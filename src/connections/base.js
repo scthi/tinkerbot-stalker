dc78a8f0c798cc367b7849e4d9f198b80c509c9e
Stephanie Feddern <s.feddern@gmail.com>
2017-09-16 14:26:57 +0200

    implement base connection add base connection as prop to base tinkerbot

---
+++ src/connections/base.js
@@ -0,0 +1,3 @@
+let mqtt = require('mqtt');
+
+module.exports = mqtt.connect('mqtt://energie-campus.cybus.io');
--- src/tinkerbots/base.js
+++ src/tinkerbots/base.js
@@ -1,6 +1,9 @@
+let broker = require('../connections/base.js');
+
 class Tinkerbot {
   constructor(id) {
     this.id = id;
+    this.broker = broker;
   }
 };
 
