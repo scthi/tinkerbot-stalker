29d0af45d9e650e16270f8b40624a7dca94ffc26
srmo <srm@schakalacka.org>
2017-09-16 16:32:04 +0200

    convert steering lock from radians to degrees

--- src/utils/curve.js
+++ src/utils/curve.js
@@ -1,9 +1,12 @@
 
-
 class Curve {
 
   calculateSteeringLock(wheelDistance, targetAngle, targetDistance) {
-    return Math.asin((2 * wheelDistance * Math.sin(targetAngle)) / targetDistance);
+    if (targetDistance == 0) {
+      throw new Error('Target length needs to be > 0');
+    }
+    let radians = Math.asin((2 * wheelDistance * Math.sin(targetAngle)) / targetDistance);
+    return radians * (180/Math.PI);
   }
 
 }
--- test/unit/utils/curve.js
+++ test/unit/utils/curve.js
@@ -5,6 +5,10 @@ let Curve = require("../../../src/utils/curve");
 describe("Curve", () => {
   describe('calculation', () => {
     let curve = new Curve();
-    assert.equal(curve.calculateSteeringLock(1,2,3), 0.6512716857674453);
+    assert.equal(curve.calculateSteeringLock(125,90,500), 26.551262509077013);
+  });
+  describe('guardAgainstDivisionByZero', () => {
+    let curve = new Curve();
+    expect(() => curve.calculateSteeringLock(1,2,0)).to.throw(Error, 'Target length needs to be > 0');
   });
 });
