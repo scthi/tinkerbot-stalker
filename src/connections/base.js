091cf5ff9849541dfa7a3634ac8472ad8cfc6fc3
srmo <srm@schakalacka.org>
2017-09-16 14:41:56 +0200

    fix connect string

--- src/connections/base.js
+++ src/connections/base.js
@@ -1,3 +1,3 @@
 let mqtt = require('mqtt');
 
-module.exports = mqtt.connect('mqtt://energie-campus.cybus.io');
+module.exports = mqtt.connect('mqtt://energie-campus.cybus.io', {username: 'wtd17.tinkerbells.for-the-win', password: 'A.edu14'});
